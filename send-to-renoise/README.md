3e1b6186100e67bd5677ee72a1084569363b1e62
bjorn-nesby <bjorn.nesby@gmail.com>
2016-04-09 20:18:57 +0200

    Initial commit

---
+++ send-to-renoise/README.md
@@ -0,0 +1,11 @@
+# Sending OSC Messages from Node.js to Renoise
+
+This example shows how to send OSC messages from a Node.js server using osc.js to Renoise.
+Example is based on Colin Clark's supercollider example for osc.js
+
+## Instructions
+
+1. Run <code>npm install</code> to install osc.js and all its dependencies.
+2. Start the Node application by running <code>node .</code>
+3. Enable the OSC server in Renoise (Preferences > OSC)
+4. Watch the OSC console for incoming OSC messages.
---
+++ send-to-renoise/index.js
@@ -0,0 +1,126 @@
+var osc = require("osc");
+
+var udpPort = new osc.UDPPort({
+    // This is the port we're listening on.
+    localAddress: "127.0.0.1",
+    localPort: 8084,
+
+    // This is where Renoise is listening for OSC messages.
+    remoteAddress: "127.0.0.1",
+    remotePort: 8004
+});
+
+// Open the socket.
+udpPort.open();
+
+console.log("process.argv",process.argv)
+
+var msg = {}
+
+switch (process.argv[2]){
+  case "hello":
+    msg = {
+        address: "/hello/from/oscjs",
+        args: [Math.random()]
+    };
+    break;
+  case "float":
+    msg = {
+      address: "/test",
+      args: {
+        type:"f",
+        value:440.4
+      }
+    }
+    break;
+  case "integer":
+    msg = {
+      address: "/test",
+      args: {
+        type:"d",
+        value:48
+      }
+    }
+    break;
+  case "number":
+    msg = {
+      address: "/test",
+      args: [{
+        type:"d",
+        value:48
+      },{
+        type:"f",
+        value:432.4
+      }]
+    }
+    break;
+  case "string":
+    msg = {
+      address: "/test",
+      args: [{
+        type:"s",
+        value:"foo"
+      },{
+        type:"s",
+        value:"bar"
+      }],
+    }
+    break;
+  case "colour":
+    msg = {
+      address: "/test",
+      args: {
+        type:"r",
+        value:new Uint8Array([255,255,255,0])
+      }
+    }
+    break;
+  case "midi":
+    msg = {
+      address: "/test",
+      args: {
+        type:"m",
+        value:new Uint8Array([0x00,0x90,0x45,0x65])
+      }
+    }
+    break;
+  case "blob":
+    msg = {
+      address: "/test",
+      args: {
+        type:"b",
+        value:new Uint8Array([0x63,0x61,0x74,0x21])
+      }
+    }
+    break;
+  case "blob":
+    msg = {
+      address: "/test",
+      args: {
+        type:"b",
+        value:new Uint8Array([0x63,0x61,0x74,0x21])
+      }
+    }
+    break;
+  // TODO
+  case "time":
+  case "float64":
+  case "integer64":
+  case "ascii":
+  case "boolean_true":
+  case "boolean_false":
+  case "nil":
+  case "inf":
+  case "alt_string":
+    break;
+  default:
+    error("Unknown message type")
+}
+
+
+// Every second, send an OSC message to Renoise
+setInterval(function() {
+
+    console.log("Sending message", msg.address, msg.args, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort);
+    udpPort.send(msg);
+}, 1000);
